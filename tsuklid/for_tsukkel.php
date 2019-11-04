fc0eda9e404d0bc2f486780166da4befa1a713bb
kasetalurene <55237873+kasetalurene@users.noreply.github.com>
2019-11-04 13:52:01 +0200

    Add files via upload

---
+++ tsuklid/for_tsukkel.php
@@ -0,0 +1,9 @@
+<?php
+
+for($paaris=1; $paaris < 11; $paaris++){
+    $jaak = $paaris % 2;
+    if ($jaak == 0) {
+
+        echo $paaris;
+    }
+}
---
+++ tsuklid/tarniruut.php
@@ -0,0 +1,19 @@
+<?php
+//ridade veerdued max
+$ridadeArv =7;
+$veergudeArv = 7;
+
+// ridade määramine ja vahetus
+
+for ($reaNR = 1; $reaNR <= $ridadeArv; $reaNR++){
+
+    echo $reaNR;
+
+  for($veeruNR = 1; $veeruNR <= $reaNR; $veeruNR++){
+
+      echo '*';
+  }
+
+    echo '<br>';
+
+}
