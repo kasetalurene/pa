1ab7d3b6c206d1eabd28582d88dacd8176f91e30
kasetalurene <55237873+kasetalurene@users.noreply.github.com>
2019-11-04 13:52:29 +0200

    Add files via upload

---
+++ Tingimus laused/operaatorid.php
@@ -0,0 +1,22 @@
+<?php
+
+$X = 3;
+$Y = 2;
+
+//loogilised operaatorid
+
+var_dump($X == $Y);
+echo '<hr>';
+var_dump($X != $Y);
+echo '<hr>';
+var_dump($X < $Y);
+echo '<hr>';
+var_dump($X > $Y);
+echo '<hr>';
+var_dump($X >= $Y);
+echo '<hr>';
+var_dump($X <= $Y);
+echo '<hr>';
+
+
+
