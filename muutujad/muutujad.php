49e2dbebd21ecdd94f9f230bdfbbbd3c3dbba071
kasetalurene <55237873+kasetalurene@users.noreply.github.com>
2019-11-04 13:53:06 +0200

    Add files via upload

---
+++ muutujad/muutujad.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+    PHP Muutujad
+    Rene Kasetalu
+    Tartu KHK
+    02.10.2019
+*/
+
+//muutujate loomine
+    $enimi = 'Karin';
+    $pnimi = 'Eegreid';
+    $nimi = $enimi.' '.$pnimi;
+    $vanus = 37;
+    $kaal = 69.5;
+    echo 'Tere ' .$nimi.'!<br>';
+    echo 'Sinu vanus on ' .$vanus.'<br>';
+    echo 'Sinu kaal on ' .$kaal.'<br>';
+
+
+
+
+
+
+?>
---
+++ muutujad/ylesanne1.php
@@ -0,0 +1,28 @@
+<?php
+
+$nimi = 'Rene';
+$sugu = 'mees';
+$vanus = 39;
+
+//väljastamine
+
+echo 'Minu nimi on ' .$nimi. '<br>';
+echo 'Olen ' .$vanus. '<br>';
+echo 'Olen ' .$sugu. '<br>'.'<br>';
+
+
+//joonistamine
+
+echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;/ '.'<br>';
+echo ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@  @' . '<br>';
+echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*' . '<br>';
+echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\""""/'.'<br>'.'<br>';
+
+
+echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\____/'.'<br>';
+echo '&nbsp;&nbsp;&nbsp;(@ , @)'.'<br>';
+echo '&nbsp;&nbsp;&nbsp;&nbsp;(&nbsp;(&nbsp;|&nbsp;|&nbsp;)&nbsp;)&nbsp;&nbsp;'.'<br>';
+echo '&nbsp;&nbsp;&nbsp;--""-""--'.'<br>';
+
+?>
+
