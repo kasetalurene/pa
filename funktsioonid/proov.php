052d77787fe57ac498e7d9919433b9713b47abc2
kasetalurene <55237873+kasetalurene@users.noreply.github.com>
2019-11-10 09:53:47 +0200

    Add files via upload

--- funktsioonid/proov.php
+++ funktsioonid/proov.php
@@ -6,4 +6,13 @@
     08.11.2019
 */
 
-
+echo date('d.m.Y G:i' , time()),'<br>';
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
+?>
